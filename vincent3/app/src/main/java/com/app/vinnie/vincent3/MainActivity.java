8e320557b356efba63f056f5fb4275170d11bbe3
vincent-dujardin <45260613+vincent-dujardin@users.noreply.github.com>
2018-12-12 10:50:10 +0100

    Add files via upload

---
+++ vincent3/app/src/main/java/com/app/vinnie/vincent3/MainActivity.java
@@ -0,0 +1,12 @@
+package com.app.vinnie.vincent3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ vincent3/app/src/main/java/com/app/vinnie/vincent3/Utils.java
@@ -0,0 +1,9 @@
+package com.app.vinnie.vincent3;
+
+public class Utils {
+
+    public static int addNumbers(int first, int second) {
+        return first + second;
+    }
+
+}
